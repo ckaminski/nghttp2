1f95aa3d5aa4cf8dec4f6184088cb4a9f8e5f1af
Chris Kaminski <gitkraken-nospam@darthcoder.com>
2017-09-20 12:20:12 -0400

    Adding sharedlib defaults and OpenSSL requirement

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,8 @@
 hunter_config(Boost VERSION 1.64.0
-    CMAKE_ARGS
-    BUILD_SHARED_LIBS=ON
+    CMAKE_ARGS 
+       BUILD_SHARED_LIBS=ON
 )
-
+hunter_config(OpenSSL VERSION 1.1.0f
+    CMAKE_ARGS 
+       BUILD_SHARED_LIBS=ON
+)
